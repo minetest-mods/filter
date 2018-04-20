66c61c8e6c30a05f1fc89fdaf29025015ebd7629
Anand S <36130650+ClobberXD@users.noreply.github.com>
2018-04-20 12:34:31 +0530

    priv check before unmuting
    Only unmutes a player if their shout priv hasn't been granted already

--- init.lua
+++ init.lua
@@ -78,11 +78,12 @@ function filter.mute(name, duration)
 
 	minetest.after(duration * 60, function()
 		muted[name] = nil
-		minetest.chat_send_player(name, "Chat privilege reinstated. Please do not abuse chat.")
-
-		local privs = minetest.get_player_privs(name)
-		privs.shout = true
-		minetest.set_player_privs(name, privs)
+ 		local privs = minetest.get_player_privs(name)
+		if privs.shout == false then
+			minetest.chat_send_player(name, "Chat privilege reinstated. Please do not abuse chat.")		
+			privs.shout = true
+			minetest.set_player_privs(name, privs)
+		end
 	end)
 end
 
